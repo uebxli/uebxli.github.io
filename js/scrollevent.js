5813c8904ed74328d8e69579baf396b3a112bd46
けろ <65333609+kero-ugok@users.noreply.github.com>
2020-06-04 14:44:16 +0900

    ページ内リンクスムーズになるよ

--- js/scrollevent.js
+++ js/scrollevent.js
@@ -1,25 +1,57 @@
 let logo = document.getElementById('logo');
 let head = document.getElementById('head');
 
-let classRemove = function (mid,rc) {
+let classRemove = function (mid, rc) {
    if (mid.classList.contains(rc)) {
       mid.classList.remove(rc);
    }
 };
 
-window.onscroll = function () {
-   let scroll = document.documentElement.scrollTop || document.body.scrollTop;;
+window.onscroll = () => {
+   let scroll = document.documentElement.scrollTop || document.body.scrollTop;
 
    if (scroll > 0) {
-      classRemove(logo,'large_logo');
+      classRemove(logo, 'large_logo');
       logo.classList.add('small_logo');
-      
+
       head.classList.add('headback');
    } else {
-      classRemove(logo,'small_logo');
+      classRemove(logo, 'small_logo');
       logo.classList.add('large_logo');
-      
-      classRemove(head,'headback');
-      console.log(head.classList);
+
+      classRemove(head, 'headback');
    }
 };
+
+let navOne = document.getElementById('one');
+let navTwo = document.getElementById('two');
+let navThr = document.getElementById('three');
+
+let uebxli = document.getElementById('uebxli');
+let uebxliY = uebxli.getBoundingClientRect().top;
+console.log('uebxliY:' + uebxliY);
+
+let plo = document.getElementById('plo');
+let ploY = plo.getBoundingClientRect().top;
+console.log('ploY:' + ploY);
+
+let headScroll = (elem, elemY) => {
+   elem.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scroll({
+         top: elemY - 70,
+         behavior: 'smooth'
+      });
+   });
+}
+
+headScroll(navOne, uebxliY);
+headScroll(navTwo, ploY);
+
+logo.addEventListener('click', (e) => {
+   e.preventDefault();
+   window.scroll({
+      top: 0,
+      behavior: 'smooth'
+   });
+});
