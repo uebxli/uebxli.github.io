b88c765f9cfdf27356c9b3d41d7c9a5c9ea1a9ae
けろ <65333609+kero-ugok@users.noreply.github.com>
2020-06-11 16:57:46 +0900

    ヘッダーのとことかレスポンシブにしたで

--- js/scrollevent.js
+++ js/scrollevent.js
@@ -1,5 +1,6 @@
 let logo = document.getElementById('logo');
 let head = document.getElementById('head');
+let phonehead = document.getElementById('phonehead');
 
 let classRemove = function (mid, rc) {
    if (mid.classList.contains(rc)) {
@@ -15,11 +16,13 @@ window.onscroll = () => {
       logo.classList.add('small_logo');
 
       head.classList.add('headback');
+      phonehead.classList.add('headback');
    } else {
       classRemove(logo, 'small_logo');
       logo.classList.add('large_logo');
 
       classRemove(head, 'headback');
+      phonehead.classList.add('headback');
    }
 };
 
@@ -27,6 +30,10 @@ let navOne = document.getElementById('one');
 let navTwo = document.getElementById('two');
 let navThr = document.getElementById('three');
 let pageTop = document.getElementById('pagetop');
+
+let hamOne = document.getElementById('hamone');
+let hamTwo = document.getElementById('hamtwo');
+let hamThr = document.getElementById('hamthree');
 
 let uebxli = document.getElementById('uebxli');
 let uebxliY = uebxli.getBoundingClientRect().top;
@@ -51,4 +58,7 @@ let headScroll = (elem, elemY) => {
 headScroll(navOne, uebxliY);
 headScroll(navTwo, ploY);
 headScroll(navThr, needY);
+headScroll(hamOne, uebxliY);
+headScroll(hamTwo, ploY);
+headScroll(hamThr, needY);
 headScroll(logo, 0);
